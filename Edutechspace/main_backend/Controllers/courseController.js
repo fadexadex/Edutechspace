f48e67def082fac9293983ec37f15e2169fd3915
Danny944 <fadehandaniel2006@gmail.com>
2025-05-20 17:19:22 +0100

    feat(course): implemented rich and lean course fetching, included columns: requirements and level

--- Edutechspace/main_backend/Controllers/courseController.js
+++ Edutechspace/main_backend/Controllers/courseController.js
@@ -1,6 +1,21 @@
 import supabase, { supabaseAdmin } from "../config/supabase.js";
 import { AppError } from "../middleware/errorHandler.js";
 
+export const getRichCourses = async (req, res) => {
+  try {
+    const { data, error } = await supabase.from("courses").select("*");
+    if (error) {
+      throw new Error(error.message);
+    }
+    if (data.length == 0) return res.json({ message: "no courses available" });
+
+    res.json(data);
+  } catch (err) {
+    console.error("getCourses: Error:", err.message);
+    res.status(400).json({ error: err.message });
+  }
+};
+
 export const getCourses = async (req, res) => {
   try {
     const { data, error } = await supabase.from("courses").select("id, name");
--- Edutechspace/main_backend/routers/courseRoutes.js
+++ Edutechspace/main_backend/routers/courseRoutes.js
@@ -2,6 +2,7 @@ import express from "express";
 import {
   enrollUserInCourse,
   getCourses,
+  getRichCourses,
   getUserEnrolledCourses,
 } from "../Controllers/courseController.js";
 import { protect } from "../middleware/authMiddleware.js";
@@ -9,7 +10,9 @@ import { protect } from "../middleware/authMiddleware.js";
 const router = express.Router();
 
 // router.get('/progress/:userId', protect, getCourseProgress);
+router.get("/list-rich", getRichCourses);
 router.get("/list", getCourses);
+
 router.post("/enroll", protect, enrollUserInCourse);
 router.get("/enrolled", protect, getUserEnrolledCourses)
 // router.post('/upload', authMiddleware, uploadResource);
