2e41730ff638500bedd4ee4082e3aaf150bcf5e6
Gasper Wonder <gasperwonder@gmai.com>
2025-05-29 19:54:15 +0100

    protected route and navbar button changes

--- Edutechspace/main_frontend/src/App.jsx
+++ Edutechspace/main_frontend/src/App.jsx
@@ -12,7 +12,7 @@ import Profile from './pages/Profile';
 import NotFound from './component/NotFoundPage';
 import CertificateExam from './pages/CertificationExam';
 import ExamPage from './ExamPages/ExamPage';
-import {frontendExam} from './ExamPages/Data/FrontendExam';
+import { frontendExam } from './ExamPages/Data/FrontendExam';
 import { dataScienceExam } from './ExamPages/Data/DataScienceExam';
 import { uiuxExam } from './ExamPages/Data/UiUxExam';
 import { backendExam } from './ExamPages/Data/BackendExam';
@@ -29,7 +29,6 @@ import UserDashboard from './pages/Dashboard';
 import ProtectedRoute from './component/ProtectedRoute';
 import AdminUploadPage from './pages/AdminUploadPage';
 
-
 const App = () => {
   const [isLoading, setIsLoading] = useState(true);
 
@@ -45,43 +44,55 @@ const App = () => {
 
   return (
     <Routes>
-      <Route path='/' element={<MainLayout />}>
+      <Route path="/" element={<MainLayout />}>
+        {/* Public Routes */}
         <Route index element={<Home />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/signup' element={<Signup />} />
-        <Route path='/course' element={<Course />} />
-        <Route path='/profile' element={<Profile />} />
-        <Route path='/dashboard' element={<UserDashboard />} />
-        <Route path='/coursedatabase' element={<CourseDatabase />} />
-        <Route path='/course/frontendcourse' element={<FrontendDevStack />} />
-        <Route path='/course/cybersecuritycourse' element={<CyberSecurityStack />} />
-        <Route path='/course/backendcourse' element={<BackendDevStack />} />
-        <Route path='/course/datasciencecourse' element={<DataScienceStack />} />
-        <Route path='/course/mlcourse' element={<MLStack />} />
-        <Route path='/course/uiuxcourse' element={<UiUxStack />} />
-        <Route path='/course/aicourse' element={<AiStack />} />
-        <Route path='/contact' element={<Contact />} />
-        <Route path='/profile' element={<Profile />} />
-        <Route path='/certification-exam' element={<CertificateExam />} />
-        <Route path='/certification-exam/frontend' element={
-          <ExamPage 
-            title={frontendExam.title}
-            instructions={frontendExam.instructions}
-            timeLimit={frontendExam.timeLimit}
-            passingScore={frontendExam.passingScore}
-            onSuccessLink={frontendExam.onSuccessLink}
-            questionsData={frontendExam.questionData}
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+
+        {/* Protected Routes (require authentication) */}
+        <Route element={<ProtectedRoute />}>
+          <Route path="/course" element={<Course />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/dashboard" element={<UserDashboard />} />
+          <Route path="/coursedatabase" element={<CourseDatabase />} />
+          <Route path="/course/frontendcourse" element={<FrontendDevStack />} />
+          <Route path="/course/cybersecuritycourse" element={<CyberSecurityStack />} />
+          <Route path="/course/backendcourse" element={<BackendDevStack />} />
+          <Route path="/course/datasciencecourse" element={<DataScienceStack />} />
+          <Route path="/course/mlcourse" element={<MLStack />} />
+          <Route path="/course/uiuxcourse" element={<UiUxStack />} />
+          <Route path="/course/aicourse" element={<AiStack />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/certification-exam" element={<CertificateExam />} />
+          <Route
+            path="/certification-exam/frontend"
+            element={
+              <ExamPage
+                title={frontendExam.title}
+                instructions={frontendExam.instructions}
+                timeLimit={frontendExam.timeLimit}
+                passingScore={frontendExam.passingScore}
+                onSuccessLink={frontendExam.onSuccessLink}
+                questionsData={frontendExam.questionData}
+              />
+            }
           />
-        } />
-        <Route path='/certification-exam/datascience' element={<ExamPage {...dataScienceExam} />} />
-        <Route path='/certification-exam/uiux' element={<ExamPage {...uiuxExam} />} />
-        <Route path='/certification-exam/backend' element={<ExamPage {...backendExam} />} />
-        <Route path='/certification-exam/cybersecurity' element={<ExamPage {...cyberSecurityExam} />} />
-        <Route path='/certification-exam/AI' element={<ExamPage {...aiExam} />} />
-        <Route path='/certification-exam/machine-learning' element={<ExamPage {...aiExam} />} />
-      {/* Direct route to admin page (no protection at router level) */}
-      <Route path="/admin/resource-upload" element={<AdminUploadPage />} />
-        <Route path='*' element={<NotFound />} />
+          <Route path="/certification-exam/datascience" element={<ExamPage {...dataScienceExam} />} />
+          <Route path="/certification-exam/uiux" element={<ExamPage {...uiuxExam} />} />
+          <Route path="/certification-exam/backend" element={<ExamPage {...backendExam} />} />
+          <Route path="/certification-exam/cybersecurity" element={<ExamPage {...cyberSecurityExam} />} />
+          <Route path="/certification-exam/AI" element={<ExamPage {...aiExam} />} />
+          <Route path="/certification-exam/machine-learning" element={<ExamPage {...aiExam} />} />
+        </Route>
+
+        {/* Admin Routes (require authentication and admin privileges) */}
+        <Route element={<ProtectedRoute requireAdmin={true} />}>
+          <Route path="/admin/resource-upload" element={<AdminUploadPage />} />
+        </Route>
+
+        {/* Catch-all for Not Found */}
+        <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
   );
